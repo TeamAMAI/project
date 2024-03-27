258befd220bf7639bd2e96546b10d89fb41e515e
Amy <amy.ibourk@gmail.com>
2024-03-27 11:51:06 +0000

    page number test 10

--- AMAI-website/script.js
+++ AMAI-website/script.js
@@ -2,19 +2,19 @@ function showQuestion2()
 {
     document.getElementById('question1').style.display = "none";
 
-    document.getElementById('question2').style.show = "block";
+    document.getElementById('question2').style.display = "block";
 }
 
 function showQuestion3()
 {
     document.getElementById('question2').style.display = "none";
 
-    document.getElementById('question3').style.show = "block";
+    document.getElementById('question3').style.display = "block";
 }
 
 function showQuestion4()
 {
     document.getElementById('question3').style.display = "none";
 
-    document.getElementById('question1').style.show = "block";
+    document.getElementById('question1').style.display = "block";
 }
